47370b7957bc0581d42c2965da2798176eb74227
jeffersonqpm <jefferson.qpm@gmail.com>
2026-01-27 20:51:19 -0300

    Inserindo exercicios

---
+++ Exercicio/Quadrado.java
@@ -0,0 +1,17 @@
+package Exercicio;
+
+public class Quadrado extends FormaGeometrica {
+
+    private double lado;
+
+    public void area() {
+
+    }
+
+    public void area(double lado) {
+
+        lado = (lado * lado);
+
+    }
+
+}
